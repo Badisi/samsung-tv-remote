6def24c5439ac9550a7a2e579257092641221d1b
Badisi <Badisi@Badisis-MacBook-Pro.local>
2017-04-27 10:47:37 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *  samsung-tv-remote
+ *  Remote client for Samsung SmartTV starting from 2016
+ *
+ *  @author Badisi
+ *  @license Released under the MIT license
+ *
+ *  https://github.com/Badisi/samsung-tv-remote
+ */
+
+module.exports = require('./lib/samsung-tv-remote.js');
