d89c6331015139447ac5908ea1c3ffd76d2f5c94
Badisi <badisi84@gmail.com>
2026-07-11 19:32:07 +0200

    refactor: tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,11 +4,17 @@ import { defineConfig, type UserConfig } from 'tsdown';
 const config: UserConfig[] = defineConfig([{
     entry: ['src/index.ts', 'src/cli.ts'],
     format: ['esm', 'cjs'],
-    dts: true,
     clean: true,
+    sourcemap: false,
+    dts: {
+        compilerOptions: {
+            declarationMap: false
+        }
+    },
     onSuccess: (): void => {
         const pkgJson = JSON.parse(readFileSync('package.json', 'utf8')) as Record<string, unknown>;
         delete pkgJson['scripts'];
+        delete pkgJson['publishConfig'];
         delete pkgJson['devDependencies'];
         writeFileSync('dist/package.json', JSON.stringify(pkgJson, null, 4));
 
